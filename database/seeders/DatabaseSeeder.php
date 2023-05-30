7a94bd8e909163c08c360fad66a23cf033166419
Fabio Camargo <fabiorcamargo@gmail.com>
2023-05-30 17:26:44 -0300

    up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
     
             $this->call([
+                PageTypeSeeder::class,
                 StatesSeed::class,
                 CitiesSeed::class
             ]);
